53aa09d552e55547b7f1a6d674b09be3796aefec
Ryan Alder <ryanalder12@gmail.com>
2018-06-10 18:27:31 -0700

    forgot the other file whoops

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const low = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileAsync');
+const adapter = new FileAsync('db.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,16 +10,22 @@ const port = process.env.PORT || 3000;
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res, next) {
-	res.status(200).render('index', {
-		//todo
-	})
+const db = low(adapter).then(function(db){
+	db.defaults({ "notes":[] }).write();
+
+	app.get('/', function(req, res, next) {
+		res.status(200).render('index', {
+			//todo
+		})
+	});
+
+	app.use(express.static('public'));
+
+	app.get('*', function(req, res) {
+		res.status(404).render('404');
+	});
+
+	app.listen(port, function() {
+		console.log(`Server is listening on port ${port}.`);
+	});
 });
-
-app.use(express.static('public'));
-
-app.get('*', function(req, res) {
-	res.status(404).render('404');
-});
-
-app.listen(port);
