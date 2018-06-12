fb3d2fad291c9ea6f15aef2afa9482a416d2fad5
Ryan Alder <ryanalder12@gmail.com>
2018-06-11 22:27:10 -0700

    Implemented adding and deleting notes

--- server.js
+++ server.js
@@ -3,21 +3,40 @@ const handlebars = require('express-handlebars');
 const low = require('lowdb');
 const FileAsync = require('lowdb/adapters/FileAsync');
 const adapter = new FileAsync('db.json');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
 
 const db = low(adapter).then(function(db){
 	db.defaults({ "notes":[] }).write();
 
 	app.get('/', function(req, res, next) {
-		dbArray = db.get('notes').value();
+		let dbArray = db.get('notes').value();
 		res.status(200).render('body', {
 			notes: dbArray
 		})
+	});
+
+	app.post('/', function(req, res) {
+		db.get('notes').push({"text":req.body.item}).write();
+		let dbArray = db.get('notes').value();
+		res.status(201).render('body', {
+			notes: dbArray
+		});
+	});
+
+	app.patch('/', function(req, res) {
+		let remove = db.get('notes').find({'text': req.body.item}).value();
+		db.get('notes').remove(remove).write();
+		let dbArray = db.get('notes').value();
+		res.status(201).render('body', {
+			notes:dbArray
+		});
 	});
 
 	app.use(express.static('public'));
